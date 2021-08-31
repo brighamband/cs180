2cabf63f02abcd5214d1915e7ce9750e4553a518
Brigham Andersen <brighamband@gmail.com>
2021-08-31 15:42:00 -0600

    ex 3 done

---
+++ Data Science Labs/lab1ex3.py
@@ -0,0 +1,43 @@
+class Car:
+  color = ''
+  model = ''
+  year = 0
+  max_speed = 0
+  mileage = 0
+  cur_speed = 0
+
+  def __init__(self, color, model, year, max_speed, mileage, cur_speed):
+    self.color = color
+    self.model = model
+    self.year = year
+    self.max_speed = max_speed
+    self.mileage = mileage
+    self.cur_speed = cur_speed
+
+  def print_speed(self):
+    print('New speed: ', self.cur_speed)
+
+  def accelerate(self, amount):
+    '''Increase current speed by 'amount' miles per hour'''
+    self.cur_speed += amount
+    self.print_speed()
+
+  def brake(self):
+    '''Decrease current speed by 20%'''
+    self.cur_speed *= 0.8
+    self.print_speed()
+
+  
+car = Car('blue', 'Corolla', 2007, 100, 40, 0)
+
+print('Speeding up.')
+while (car.cur_speed + 10) <= car.max_speed:
+  car.accelerate(10)
+
+print('Max speed reached. Slowing back down.')
+car.brake()
+while car.cur_speed > 0:
+  car.cur_speed -= 10
+  car.print_speed()
+
+print('Car has stopped')
