95b76853fada15324c4920ce6ad751a2be7221f5
Brigham Andersen <brighamband@gmail.com>
2021-09-10 13:04:12 -0600

    finished io and mean

---
+++ Python Labs/lab4.py
@@ -0,0 +1,30 @@
+import argparse
+
+def calc_mean(array):
+  total = 0
+  for num in array:
+    total += num
+  return total / len(array)
+
+def calc_variance(array):
+  return 0.0
+
+# Mean and variance lab
+# Given an array as input, output its mean and variance
+def main(array):
+  mean = calc_mean(array)
+  variance = calc_variance(array)
+  print(f'mean = {mean}')
+  print(f'variance = {variance}')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--array", nargs="+", type=int, help="An array of which you want to know the mean and variance")
+    args = parser.parse_args()
+
+    if (args.array is None):
+      print("Make sure to pass in an array")
+      quit()
+
+    main(args.array)
+
