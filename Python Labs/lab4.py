7ff409100e74c4af9bc8521416eb0b0c8eeda882
Brigham Andersen <brighamband@gmail.com>
2021-09-10 13:08:02 -0600

    Finished lab 4

--- Python Labs/lab4.py
+++ Python Labs/lab4.py
@@ -6,14 +6,17 @@ def calc_mean(array):
     total += num
   return total / len(array)
 
-def calc_variance(array):
-  return 0.0
+def calc_variance(array, mean):
+  square_diff = 0
+  for i in range(len(array)):
+    square_diff += (array[i] - mean) * (array[i] - mean)
+  return square_diff / len(array);
 
 # Mean and variance lab
 # Given an array as input, output its mean and variance
 def main(array):
   mean = calc_mean(array)
-  variance = calc_variance(array)
+  variance = calc_variance(array, mean)
   print(f'mean = {mean}')
   print(f'variance = {variance}')
 
