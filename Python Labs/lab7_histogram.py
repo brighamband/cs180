a5adef5a7487db1ac5dfff8a36b560cda7b77a0c
Brigham Andersen <brighamband@gmail.com>
2021-09-21 22:02:18 -0600

    finished lab 7

--- Python Labs/lab7_histogram.py
+++ Python Labs/lab7_histogram.py
@@ -1,30 +1,64 @@
 import argparse
 
-def find_slot(num, bins):
-  pass
-
-def main(array, num_bins):
+def print_inputs(array, num_bins):
   print('a =', array)
   print('b =', num_bins)
   print()
 
-  highest = max(array)
-  lowest = min(array)
-  bin_width = (highest - lowest + 1) / num_bins
-  bin_width = round(bin_width, 1) # Round to nearest tenths
 
-  bins = [0] * (num_bins)
+def calc_bin_width(highest_num, lowest_num, num_bins):
+  width = (highest_num - lowest_num + 1) / num_bins
+  width = round(width, 1) # Round to nearest tenths
+  return width
 
+
+def make_bins(array, num_bins, bin_width, lower_bound, upper_bound):
+  bins = [0] * (num_bins) # Initialize bins (as many as num_bins)
+
+  for i in range(len(bins)):
+      # Populate bins
+      for num in array:
+        if num >= lower_bound and num < upper_bound: 
+          bins[i] += 1
+
+      lower_bound, upper_bound = update_bounds(lower_bound, upper_bound, bin_width)
+  
+  return bins
+
+
+''' 
+Updates lower and upper bounds for the next bin.
+lower_b - Lower bound (inclusive '[').
+upper_b - Upper bound (exclusive ')').
+'''
+def update_bounds(lower_b, upper_b, bin_width):
+  lower_b = upper_b
+  upper_b = round(upper_b + bin_width, 1) # Round to nearest tenths
+  return lower_b, upper_b
+
+
+def print_bins(bins, bin_width, lower_bound, upper_bound):
+  for bin in bins:
+    print('# [' + str(lower_bound) + ',' + str(upper_bound) + '):', bin)  
+    lower_bound, upper_bound = update_bounds(lower_bound, upper_bound, bin_width)
+
+
+def main(array, num_bins):
+  # print_inputs(array, num_bins)
+
+  # Initialize values
+  highest_num = max(array)
+  lowest_num = min(array)
+  bin_width = calc_bin_width(highest_num, lowest_num, num_bins)
+  init_lower_b = lowest_num # Initial lower bound 
+  init_upper_b = lowest_num + bin_width # Initial upper bound
+
+  bins = make_bins(array, num_bins, bin_width, init_lower_b, init_upper_b)
+ 
   print(bins)
   print()
 
-  lower_bound = lowest # Inclusive '['
-  upper_bound = lower_bound + bin_width # Exclusive ')'
-
-  for bin in bins:
-    print('# [' + str(lower_bound) + ',' + str(upper_bound) + '):', bin)
-    lower_bound = upper_bound
-    upper_bound = round(upper_bound + bin_width, 1) # Round to nearest tenths
+  print_bins(bins, bin_width, init_lower_b, init_upper_b)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
